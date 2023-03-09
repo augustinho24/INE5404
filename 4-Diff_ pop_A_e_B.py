a7b5a81a68ff9862928b6b4c06dc23b4f7587f84
augustinho24 <105183187+augustinho24@users.noreply.github.com>
2023-03-09 11:29:26 -0300

    Add files via upload

---
+++ 4-Diff_ pop_A_e_B.py
@@ -0,0 +1,9 @@
+def diff(x,y):
+    calc = x*0.03 - y*0.015
+    return calc * -1
+
+A = 80000
+B = 200000
+
+print(diff(A,B))
+
---
+++ 7-maior_num.py
@@ -0,0 +1,5 @@
+num = int(input()).split( )
+maior = 0 
+for num in range(len(num+1)):  
+    if num > maior: 
+        maior = num
