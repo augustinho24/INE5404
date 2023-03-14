cd16d35db89c40e97af26809dc27b4affd24b23e
augustinho24 <105183187+augustinho24@users.noreply.github.com>
2023-03-14 09:43:14 -0300

    Update 4-Diff_ pop_A_e_B.py

--- Exercícios Iniciais POOII/4-Diff_ pop_A_e_B.py
+++ Exercícios Iniciais POOII/4-Diff_ pop_A_e_B.py
@@ -1,9 +1,15 @@
-def diff(x,y):
-    calc = x*0.03 - y*0.015
-    return calc * -1
 
 A = 80000
 B = 200000
+anos = 0
+while True:
+    anos +=1
+    A += A*0.03
+    B += A*0.015
+    if A > B:
+        break
 
-print(diff(A,B))
+print(anos)
+
+
 
