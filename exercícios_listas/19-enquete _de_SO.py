cc1369ee385182847de8994667b3f5e15868efad
augustinho24 <guiolivera2004@hotmail.com>
2023-05-04 00:53:12 -0300

    Changes to be committed: 	modified:   "exerc\303\255cios_listas/19-enquete _de_SO.py"

--- exercícios_listas/19-enquete _de_SO.py
+++ exercícios_listas/19-enquete _de_SO.py
@@ -7,26 +7,19 @@ while True:
     while voto < 0 or voto > 6:
         voto = int(input('Informe um valor entre 1 e 6 ou 0 para sair!'))
 
-        if voto in votos_enquete:
+        if voto in votos_enquete.keys():
             votos_enquete[voto] += 1
         else:
             votos_enquete[voto] = 1
 
 
-'''print('Resultado da votação:\n')
-print('Foram computados', len(jogadores), 'votos\n')
-print('Jogador\tVotos\t%')
-for u,v in jogadores.items():
-    print(f'{u}\t{v}\t{round((v/len(jogadores))*100,2)}%')
+total_votos = sum(votos_enquete.values())
+if total_votos == 0:
+    total_votos = 1
 
-print('\nO melhor jogador foi o número', max(jogadores, key=lambda key: jogadores[key]), 'com', max(jogadores.values()), 'votos, correspondendo a', round((max(jogadores.values())/len(jogadores))*100,2), '% do total de votos.')
 
-'''
+print('Total                    ', total_votos)
 
-print('Sistema Operacional\tVotos\t%')
-print('Windows Server\t\t', votos_enquete.get(1, 0), '\t', round((votos_enquete.get(1, 0)/sum(votos_enquete.values()))*100,2), '%')
-print('Unix\t', votos_enquete.get(2, 0), '\t', round((votos_enquete.get(2, 0)/sum(votos_enquete.values()))*100,2), '%')   
-print('Linux\t', votos_enquete.get(3, 0), '\t', round((votos_enquete.get(3, 0)/sum(votos_enquete.values()))*100,2), '%')   
-print('Netware\t', votos_enquete.get(4, 0), '\t', round((votos_enquete.get(4, 0)/sum(votos_enquete.values()))*100,2), '%')   
-print('Mac OS\t', votos_enquete.get(5, 0), '\t', round((votos_enquete.get(5, 0)/sum(votos_enquete.values()))*100,2), '%')   
-print('Outra\t', votos_enquete.get(6, 0), '\t', round((votos_enquete.get(6, 0)/sum(votos_enquete.values()))*100,2), '%')  
+#ARRUMAR DEPOIS
+
+# print('O Sistema Operacional mais votado foi o', max(votos_enquete, key=lambda key: votos_enquete[key]), 'com', max(votos_enquete.values()), 'votos, correspondendo a', round((max(votos_enquete.values())/total_votos)*100,2), '% do total de votos.')
